7ce82f67e933e5abeb0d96a1424c3b3efbb8d809
sandeep_zachariah <sandeep.zachariah@oracle.com>
2020-02-18 00:11:13 +0530

    implement game and player repo

--- src/main/java/com/game/tictactoe/jpa/dao/GameRepo.java
+++ src/main/java/com/game/tictactoe/jpa/dao/GameRepo.java
@@ -1,8 +1,11 @@
 package com.game.tictactoe.jpa.dao;
 
+import com.game.tictactoe.jpa.dto.Game;
+import org.springframework.data.jpa.repository.JpaRepository;
+
 /**
  * repository interface for games
  */
 
-public interface GameRepo {
+public interface GameRepo extends JpaRepository<Game, Integer> {
 }
--- src/main/java/com/game/tictactoe/jpa/dao/PlayerRepo.java
+++ src/main/java/com/game/tictactoe/jpa/dao/PlayerRepo.java
@@ -1,8 +1,11 @@
 package com.game.tictactoe.jpa.dao;
 
+import com.game.tictactoe.jpa.dto.Player;
+import org.springframework.data.jpa.repository.JpaRepository;
+
 /**
  * repository interface for players
  */
 
-public interface PlayerRepo {
+public interface PlayerRepo extends JpaRepository<Player, Integer> {
 }
