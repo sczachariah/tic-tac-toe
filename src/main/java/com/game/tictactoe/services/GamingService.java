70898b7272e65f69cb81446c2e7c05370c742636
sandeep_zachariah <sandeep.zachariah@oracle.com>
2020-02-18 00:32:21 +0530

    define gaming service interface

--- src/main/java/com/game/tictactoe/services/GamingService.java
+++ src/main/java/com/game/tictactoe/services/GamingService.java
@@ -1,11 +1,28 @@
 package com.game.tictactoe.services;
 
-import org.springframework.stereotype.Service;
+import com.game.tictactoe.jpa.dto.Game;
+import com.game.tictactoe.jpa.dto.Player;
+
+import java.util.List;
 
 /**
- * service for gaming rest api
+ * service interface for gaming rest api
  */
 
-@Service
-public class GamingService {
+public interface GamingService {
+    Game createGame(String playerName);
+
+    List<Game> listAllGames();
+
+    Game joinGame(int id, String playerName);
+
+    boolean performGameMove(int id, String playerName, String move);
+
+    Object getGameState(int id);
+
+    Object getGameStatus(int id);
+
+    Player getGameWinner(int id);
+
+    boolean endGame(int id);
 }
